e698e856b46dbe421599e373cf57a44172056ca6
ArtemMelkevych <a.melkevych@kyivstar.net>
2020-10-30 13:33:14 +0200

    first commit

---
+++ iosCourse/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iosCourse
+//
+//  Created by Artem Melkevych on 30.10.2020.
+//  Copyright © 2020 Artem Melkevych. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
